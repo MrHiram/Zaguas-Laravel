afe93ef40fe03d325366fe2282b18f0b008fd60a
Jean Brenes <jlean30@protonmail.com>
2019-10-11 00:48:23 -0600

    basic functions api
    Login, register, get data

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::group(['middleware' => ['json.response']], function () {
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // public routes
+    Route::post('/login', 'AuthController@login')->name('login.api');
+    Route::post('/register', 'AuthController@register')->name('register.api');
+
+    // private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'AuthController@logout')->name('logout');
+    });
+
 });
