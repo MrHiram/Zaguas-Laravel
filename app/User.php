7190d587b0985a6eba0f7c97c0ac2c0fd7d874c7
Jean Brenes <jlean30@protonmail.com>
2019-10-14 15:08:28 -0600

    change register

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','active', 'activation_token'
+        'name','lastname', 'email', 'password','active', 'activation_token'
     ];
 
     /**
